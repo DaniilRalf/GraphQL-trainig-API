f418848f555238de0a27a9a7e158424f0908e047
DaniilRalf <ralf.danya@mail.ru>
2022-12-24 18:21:58 +0700

    exam_3

---
+++ index_exam_3.js
@@ -0,0 +1,105 @@
+import {ApolloServer} from "apollo-server";
+
+/**
+ * В этой конфигурации рассматривается связь один ко многим
+ * Для этого внедрим еще одну сущбность User
+ * User имеет некоторое кол-во Photo
+ * Добавим данные для наглядности
+ * */
+
+let _id = 1
+let users = [
+    {githubLogin: "login1", name: 'name1'},
+    {githubLogin: "login2", name: 'name2'},
+    {githubLogin: "login3", name: 'name3'},
+]
+/**
+ * githubUser - поле показыающее связь photos и users
+ * Нет необходимости добавлять в Схему, поскольку на практике его нет необходимости передавать на клиент
+ * */
+let photos = [
+    {id: 1, name: 'name1', description: 'desc1', category: 'ACTION', githubUser: 'login2'},
+    {id: 1, name: 'name1', description: 'desc1', category: 'ACTION', githubUser: 'login2'},
+    {id: 1, name: 'name1', description: 'desc1', category: 'ACTION', githubUser: 'login3'},
+]
+
+
+const typeDefs = `
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+        category: PhotoCategory!
+        postedBy: User!
+    }
+    type User {
+        githubLogin: ID!
+        name: String
+        avatar: String
+        postedPhotos: [Photo!]!
+    }
+    
+ 
+    input PhotoCategoryInput {
+        name: String!
+        description: String
+        category: PhotoCategory=PORTRAIT
+    }
+    
+    
+    enum PhotoCategory {
+        SELFIE
+        PORTRAIT
+        ACTION
+        LANDSCAPE
+        GRAPHIC
+    }
+    
+    
+    type Query {
+        totalPhotos: Int!
+        allPhotos: [Photo!]!
+    }
+    type Mutation {
+        postPhoto(input: PhotoCategoryInput!): Photo!
+    }
+`
+
+const resolvers = {
+    Photo: {
+        url: parent => `http://sadfasdf/${parent.id}.com`,
+        postedBy: parent => {
+            return users.find(u => u.githubLogin === parent.githubUser)
+        }
+    },
+    User: {
+        postedPhotos: parent => {
+            return photos.find(p => p.githubUser === parent.githubLogin)
+        }
+    },
+
+
+    Query: {
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos,
+    },
+    Mutation: {
+        postPhoto(parent, args) {
+            let newPhoto = {
+                id: _id++,
+                ...args.input
+            }
+            photos.push(newPhoto)
+            return newPhoto
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+})
+server.listen().then(data => {
+    console.log(`GraphGQ start on ${data.url}`)
+})
